b8a78632fb5c6152b6a21de72b15c95de097a20e
Jim Void <8578834+YasusBeebu@users.noreply.github.com>
2021-10-17 16:01:55 +0400

    create drawField

--- js/baseJs.js
+++ js/baseJs.js
@@ -1,3 +1,28 @@
-.leftMenu {
-    
+const drawContainer = findElement("#drawContainer");
+// shortem querrySelector
+function findElement(selector, all = false) {
+    if (all) {
+        return document.querySelectorAll(selector);
+    }
+    return document.querySelector(selector);
 }
+// Create a Node and add a class to it
+function createNode(element = "div", cssCLass = "pixel") {
+    const div = document.createElement(element);
+    div.classList.add('pixel');
+    return div;
+};
+//Drawing field 
+function drawField(num) {
+    for (let i = num; i > 0; i--) {
+        let pixel = createNode('div');
+        for (let y = num * 2; y > 0; y--) {
+            pixel.append(createNode("div"))
+        }
+        drawContainer.append(pixel);
+    }
+}
+//----------------------------------------------------------
+
+
+drawField(64)
