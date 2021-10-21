05e67c73fa7e84dd38e0e91abfc3a48227667938
Jim Void <8578834+YasusBeebu@users.noreply.github.com>
2021-10-21 21:30:13 +0400

    add basic js logic

--- js/baseJs.js
+++ js/baseJs.js
@@ -4,33 +4,42 @@ function findElement(selector, all = false) {
         return document.querySelectorAll(selector);
     }
     return document.querySelector(selector);
-}
+};
+
 // Create a Node and add a class to it
-function createNode(element = "div", cssCLass = "pixel") {
+function createNode(element = 'div', cssCLass = 'row') {
     const div = document.createElement(element);
-    div.classList.add('pixel');
+    div.classList.add(cssCLass);
     return div;
 };
-// Drawing field with divs
+
+// Drawing field with divs in drawContainer.
 function drawGrid(num) {
     for (let i = num; i > 0; i--) {
-        let pixel = createNode('div');
-        for (let y = num * 2; y > 0; y--) {
-            pixel.append(createNode("div"))
-        }
-        findElement("#drawContainer").append(pixel);
-    }
-}
+        let row = createNode('div');
+        for (let y = num * 3; y > 0; y--) { //Amount cells in rows, x3 coz drawContainer is a Rectangle not a Square
+            let div = createNode('div');
+            div.classList = 'rowCell';
+            row.append(div);
 
-function defaultGrid() {
-    drawContainer.innerText = "";
-    drawGrid(16);
-}
+        };
+        findElement('#drawContainer').append(row);
+    };
+};
+// Draw new grid in the drawContainer and add mouseover events with a change to the bg color
+function newGrid(size = 16) {
+    drawContainer.innerText = '';
+    drawGrid(size);
+    changeBgcolor('black')
+};
 
-function zIndex(nodelist, hide = 0) {
-    nodelist.forEach(element => {
-        element.style.zIndex = hide;
+function changeBgcolor(bgColor = "black", random = false) {
+    const grid = findElement('.rowCell', 1); // find gridCells and add paint event
+    const drawContainer = findElement('#drawContainer');
+
+    grid.forEach(element => {
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = bgColor;
+        });
     });
 }
-
-//----------------------------------------------------------
